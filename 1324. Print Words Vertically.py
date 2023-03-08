8af5796e74b153de4d235c875a7c837c122e634e
Saurabh Patel <64309848+saurabhpatel7@users.noreply.github.com>
2023-03-08 16:52:25 +0530

    Create 1324. Print Words Vertically.py

---
+++ 1324. Print Words Vertically.py
@@ -0,0 +1,7 @@
+class Solution:
+    def maximum69Number (self, num: int) -> int:
+        num_str = str(num)
+        for i in range(len(num_str)):
+            if num_str[i] == '6':
+                return int(num_str[:i] + '9' + num_str[i+1:])
+        return num
